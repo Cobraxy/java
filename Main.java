774ba562fc3184db07ba2c5e01d23c4b33d60fa0
George <admin@gmail.com>
2022-12-25 03:18:29 +0500

    Add file

--- Main.java
+++ Main.java
@@ -3,7 +3,7 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("ok");
+		System.out.println("Hello");
 	}
 
 }
--- com/Some.java
+++ com/Some.java
@@ -1,5 +1,7 @@
 package com;
 
 public class Some {
-
+	public void open() {
+		System.out.println("Hola mundo!");
+	}
 }
